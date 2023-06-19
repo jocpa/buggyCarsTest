8091cf0085a28c6bebbe10d0732d8c9b9d06dff9
Pablo Arzu <jptomas38@gmail.com>
2023-06-19 08:30:17 -0600

    registerUser.js: elementos creados

--- cypress/pages/registerUser.js
+++ cypress/pages/registerUser.js
@@ -1,6 +1,15 @@
 
 class registerUser {
 
-    
+    elements = {
+        registerBtn: () => cy.get('.btn-success-outline'),
+        registerText: () => cy.get('h2'),
+        inputLogin: () => cy.get('#username'),
+        inputFirsName: () => cy.get('#firstName'),
+        inputLastName: () => cy.get('#lastName'),
+        inputPassword: () => cy.get('#password'),
+        defaultBtn: () => cy.get('.btn-default')
+        
+    }
 
 }
