4710ad46cf15328c31d4a5963197cea9c3bd4009
Pablo Arzu <jptomas38@gmail.com>
2023-06-19 08:23:08 -0600

    smokeTest.cy.js creado

--- cypress/e2e/smokeTest.cy.js
+++ cypress/e2e/smokeTest.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
+describe('Prueba de humo Buggy cars', () => {
+
   it('Deberia cargar la pagina correctamente', () => {
+
     cy.visit('https://buggy.justtestit.org/')
+    cy.get('.navbar-brand').should('exist')
+    cy.get('.display-4').invoke('text').should('contains', 'BuggyCarsRating')
+    cy.url().should('eq', 'https://buggy.justtestit.org/')
+    
   })
+
 })
