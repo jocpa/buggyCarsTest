522e9c1f8d5a4070f1c9910270e07b6cb670610d
Pablo Arzu <jptomas38@gmail.com>
2023-06-19 09:09:59 -0600

    updateUser.js: elementos creados

--- cypress/pages/uptdateUser.js
+++ cypress/pages/uptdateUser.js
@@ -1,5 +1,20 @@
 class updateUser {
 
-    
+    elemets = {
+        profileBnt: () => cy.get(':nth-child(2) > .nav-link'),
+        firstName: () => cy.get('#firstName'),
+        lastName: () => cy.get('#lastName'),
+        selectGender: () => cy.get('#gender'),
+        age: () => cy.get('#age'),
+        adress: () => cy.get('#address'),
+        phoneNymber: () => cy.get('#phone'),
+        selectHobyy: () => cy.get('#hobby'),
+        currentPassword: () => cy.get('#currentPassword'),
+        newPassword: () => cy.get('#newPassword'),
+        confirmPassword: () => cy.get('#newPasswordConfirmation'),
+        selectLanguage: () => cy.get('#language'),
+        saveBtn: () => cy.get('.btn-default')
+    }
 
 }
+module.exports = new updateUser();
